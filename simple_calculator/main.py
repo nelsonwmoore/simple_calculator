c3c78e16b859d0a163bd65409ee1fca1ab63dc51
Nelson Moore <nelson.moore@essential-soft.com>
2024-05-21 17:56:13 -0400

    feat: add SimpleCalculator with add, sub, mul
    - add Simple Calculator class with methods to add, subtract, and multiple numbers.
    - add tests for above methods
    - add venv to .gitignore

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,20 @@
-# -*- coding: utf-8 -*-
+"""Main module for the simple_calculator package."""
+
+import operator
+from functools import reduce
+
+
+class SimpleCalculator:
+    """A simple calculator class."""
+
+    def add(self, *args: int) -> int:
+        """Add numbers."""
+        return sum(args)
+
+    def sub(self, a: int, b: int) -> int:
+        """Subtract b from a."""
+        return a - b
+
+    def mul(self, *args: int) -> int:
+        """Multiply numbers."""
+        return reduce(operator.mul, args)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,47 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
+from simple_calculator.main import SimpleCalculator
+
+
+def test_add_two_numbers() -> None:
+    calculator = SimpleCalculator()
+    actual = calculator.add(4, 5)
+    expected = 9
+    assert actual == expected
+
+
+def test_add_three_numbers() -> None:
+    calculator = SimpleCalculator()
+    actual = calculator.add(4, 5, 6)
+    expected = 15
+    assert actual == expected
+
+
+def test_add_many_numbers() -> None:
+    numbers = range(100)
+    calculator = SimpleCalculator()
+    actual = calculator.add(*numbers)
+    expected = 4950
+    assert actual == expected
+
+
+def test_sub_two_numbers() -> None:
+    calculator = SimpleCalculator()
+    actual = calculator.sub(10, 3)
+    expected = 7
+    assert actual == expected
+
+
+def test_mul_two_numbers() -> None:
+    calculator = SimpleCalculator()
+    actual = calculator.mul(4, 5)
+    expected = 20
+    assert actual == expected
+
+
+def test_mul_many_numers() -> None:
+    numbers = range(1, 10)
+    calculator = SimpleCalculator()
+    actual = calculator.mul(*numbers)
+    expected = 362880
+    assert actual == expected
